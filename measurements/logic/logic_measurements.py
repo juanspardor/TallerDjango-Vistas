e31e162f80ed778c8ceb413a85445a355fed9e3f
juanspardor <j.pardor@uniandes.edu.co>
2023-02-12 15:01:23 -0500

    Cambios finales, incluye pruebas Postman

--- measurements/logic/logic_measurements.py
+++ measurements/logic/logic_measurements.py
@@ -1,4 +1,7 @@
 from ..models import Measurement
+from ..models import Variable
+from django.db import models
+
 
 def get_measurements():
     measurements = Measurement.objects.all()
@@ -9,16 +12,22 @@ def get_measurement(mer_pk):
     return measurement
 
 def create_measurement(mer):
-    measurement = Measurement(unit = mer["unit"], value = mer["value"], place = mer["place"], dateTime = mer["dateTime"], variable = mer["variable"])
+    pVariable = Variable.objects.get(pk=mer["variable"])
+    measurement = Measurement(unit = mer["unit"], value = mer["value"], place = mer["place"], dateTime = mer["dateTime"], variable = pVariable)
     measurement.save()
     return measurement
 
 def update_measurement(mer_pk, mer):
+    pVariable = Variable.objects.get(pk=mer["variable"])
     measurement = get_measurement(mer_pk)
     measurement.unit = mer["unit"]
     measurement.value = mer["value"]
     measurement.place = mer["place"]
     measurement.dateTime = mer["dateTime"]
-    measurement.variable = mer["variable"]
+    measurement.variable = pVariable
     measurement.save()
+    return measurement
+
+def delete_measurement(mer_pk):
+    measurement = Measurement.objects.get(pk = mer_pk).delete()
     return measurement
--- measurements/views.py
+++ measurements/views.py
@@ -20,16 +20,19 @@ def measurements_view(request):
             return HttpResponse(measurements, 'application/json')
       
     if request.method =='POST':
-       measurement_dto = ml.create_measurement(json.loads(request.body))
-       measurement = serializers.serialize('json', [measurement_dto])
-       return HttpResponse(measurement, 'application/json')
-    
+        variable_dto = ml.create_measurement(json.loads(request.body))
+        variable = serializers.serialize('json', [variable_dto,])
+        return HttpResponse(variable, 'application/json')
 @csrf_exempt
 def measurement_view(request, pk):
     if request.method == 'GET':
         measurement_dto = ml.get_measurement(pk)
-        measurement = serializers.serialize('json', [measurement_dto])
+        measurement = serializers.serialize('json', [measurement_dto,])
         return HttpResponse(measurement, 'application/json')
+    
+    if request.method == 'DELETE':
+        measurement_dto = ml.delete_measurement(pk)
+        return HttpResponse(status = 200)
     
     if request.method == 'PUT':
         measurement_dto = ml.update_measurement(pk, json.loads(request.body))
